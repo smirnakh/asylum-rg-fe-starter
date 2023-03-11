662f1c704e051f3435207b58083b74ccdb70a6f8
smirnakh <99379073+smirnakh@users.noreply.github.com>
2023-03-11 10:03:54 -0800

    added a description section in the bottom section

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -83,7 +83,32 @@ function RenderLandingPage(props) {
       <div className="bottom-section">
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
         {/* <div className="bottom-section">*/}
-
+        <h1>Systemic Disparity Insights</h1>
+        <div className="description-section">
+          <div>
+            <h2>36%</h2>
+            <p>
+              By the end of Trump administration, the average asylum office
+              grant rate had fallen 36 percent from an average of 44 percent in
+              fiscal year 2016 to 28 percent in fiscal year 2020
+            </p>
+          </div>
+          <div>
+            <h2>5%</h2>
+            <p>
+              The New York asylum office grant rate dropped to 5 percent in
+              fiscal year 2020.
+            </p>
+          </div>
+          <div>
+            <h2>6X Lower</h2>
+            <p>
+              Between fiscal year 2017 and 2020, the New York asylum office's
+              average grant rate was six times lower than the San Francisco
+              asylum office.
+            </p>
+          </div>
+        </div>
         <Button
           type="default"
           style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
